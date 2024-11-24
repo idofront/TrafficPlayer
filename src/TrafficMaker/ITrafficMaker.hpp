a6f568dcbad94f8e1ebbd106e75a6648ae9ebae1
Kumazawa <kumazawa@ic.idofront.com>
2024-11-25 00:17:57 +0900

    ITrafficMaker の宣言と実装を分離

--- src/TrafficMaker/ITrafficMaker.hpp
+++ src/TrafficMaker/ITrafficMaker.hpp
@@ -17,34 +17,8 @@ class ITrafficMaker
     virtual std::vector<TrafficRecord> Make() = 0;
 
   protected:
-    std::filesystem::path Path() const
-    {
-        return _path;
-    }
-
-    virtual std::vector<PcapRecord> ReadPcapFile(const std::filesystem::path &path) final
-    {
-        auto pcapRecords = std::vector<PcapRecord>();
-
-        auto pcap = pcap_open_offline(path.c_str(), NULL);
-        if (pcap == NULL)
-        {
-            throw std::runtime_error("Failed to open pcap file");
-        }
-
-        struct pcap_pkthdr header;
-        const u_char *packet;
-        while ((packet = pcap_next(pcap, &header)) != NULL)
-        {
-            std::vector<uint8_t> data(packet, packet + header.len);
-            auto record = PcapRecord(header, data);
-            pcapRecords.push_back(record);
-        }
-
-        pcap_close(pcap);
-
-        return pcapRecords;
-    }
+    std::filesystem::path Path() const;
+    virtual std::vector<PcapRecord> ReadPcapFile(const std::filesystem::path &path) final;
 
   private:
     std::filesystem::path _path;
