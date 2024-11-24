391e30e8d1f8babc5b2388a300e5809ede2fbe00
Kumazawa <kumazawa@ic.idofront.com>
2024-11-24 20:11:32 +0900

    SpeedScale モードで送信後の待ち時間が積算されてしまっているバグを修正

--- src/TrafficMaker/SpeedScaledReplayTrafficMaker.cpp
+++ src/TrafficMaker/SpeedScaledReplayTrafficMaker.cpp
@@ -18,19 +18,23 @@ std::vector<TrafficRecord> SpeedScaleRepalyTrafficMaker::Make()
                    [](const PcapRecord &record) { return record.Ts().tv_sec * 1e6 + record.Ts().tv_usec; });
 
     auto maxTimestamp = *std::max_element(usecTimestamps.begin(), usecTimestamps.end());
-    auto minTimestamp = *std::min_element(usecTimestamps.begin(), usecTimestamps.end());
 
-    std::transform(pcapRecords.begin(), pcapRecords.end(), std::back_inserter(trafficRecords),
-                   [minTimestamp, maxTimestamp, speedScaleFactor](const PcapRecord &record) {
-                       auto data = record.Data();
-                       auto usecTimeval = record.Ts().tv_sec * 1e6 + record.Ts().tv_usec;
+    for (auto i = 0; i < pcapRecords.size(); i++)
+    {
+        auto targetRecord = pcapRecords[i];
+        auto targetUsecTimestamp = targetRecord.Ts().tv_sec * 1e6 + targetRecord.Ts().tv_usec;
+        auto scaledTargetUsecTimestamp = targetUsecTimestamp / speedScaleFactor;
+        auto nextUsecTimeStamp = (i + 1 < pcapRecords.size())
+                                     ? pcapRecords[i + 1].Ts().tv_sec * 1e6 + pcapRecords[i + 1].Ts().tv_usec
+                                     : maxTimestamp;
+        auto scaledNextUsecTimestamp = nextUsecTimeStamp / speedScaleFactor;
 
-                       auto relativeUsecTimestamp = usecTimeval - minTimestamp;
-                       auto expectedInterval = relativeUsecTimestamp / speedScaleFactor;
+        auto scaledChronoNano = std::chrono::nanoseconds(
+            static_cast<long long>((scaledNextUsecTimestamp - scaledTargetUsecTimestamp) * 1e3));
 
-                       auto scaledChronoNano = std::chrono::nanoseconds(static_cast<long long>(expectedInterval * 1e3));
-                       return TrafficRecord(data, scaledChronoNano);
-                   });
+        auto trafficRecord = TrafficRecord(targetRecord.Data(), scaledChronoNano);
+        trafficRecords.push_back(trafficRecord);
+    }
 
     return trafficRecords;
 }
